091412a2f5aaaee739f00d2a31c81f3162fbf9dc
rubialburqueque <rubi.vap@gmail.com>
2020-04-15 11:39:17 -0500

    vistas consultora

---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="text-center">
+  <div class="container">
+    <header class="blog-header py-3">
+      <div class="row flex-nowrap justify-content-between align-items-center">
+        <div class="col-4 pt-1">
+        <hamburguer/>
+        </div>
+        <div class="col-4 text-center">
+          <img  class="blog-header-logo text-dark" alt="Vue logo" src="../assets/logo.png">
+        </div>
+        <div class="col-4 d-flex justify-content-end align-items-center">
+        </div>
+      </div>
+    </header>
+    <div class="nav-scroller py-1 mb-2">
+      <nav class="nav d-flex justify-content-between">
+        <a class="p-2 text-muted" href="#">Vista1</a>
+        <a class="p-2 text-muted" href="#">Vista2</a>
+        <a class="p-2 text-muted" href="#">Vista3</a>
+        <a class="p-2 text-muted" href="#">Vista4</a>
+        <a class="p-2 text-muted" href="#">Vista5</a>
+        <a class="p-2 text-muted" href="#">Vista6</a>
+        <a class="p-2 text-muted" href="#">Vista7</a>
+        <a class="p-2 text-muted" href="#">Vista8</a>
+        <a class="p-2 text-muted" href="#">Vista9</a>
+        <a class="p-2 text-muted" href="#">Vista10</a>
+        <a class="p-2 text-muted" href="#">Vista11</a>
+        <a class="p-2 text-muted" href="#">Vista12</a>
+      </nav>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import Hamburguer from './Hamburguer.vue';
+
+export default {
+  name: 'Header',
+  components: {
+    Hamburguer,
+  },
+};
+</script>
+
+<style scoped>
+.blog-header {
+    line-height: 1;
+    border-bottom: 1px solid
+    #e5e5e5;
+};
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,24 @@
 <template>
   <div class="home">
-    <div class="text-center">
-      <!--header-->
-      <div class="container">
-        <header class="blog-header py-3">
-          <div class="row flex-nowrap justify-content-between align-items-center">
-            <div class="col-4 pt-1">
-              <a class="text-muted" href="#">Inicio</a>
-            </div>
-            <div class="col-4 text-center">
-              <img  class="blog-header-logo text-dark" alt="Vue logo" src="../assets/logo.png">
-            </div>
-            <div class="col-4 d-flex justify-content-end align-items-center">
-              <a class="btn btn-sm btn-outline-secondary" href="#">Sign In</a>
-            </div>
-          </div>
-        </header>
-      </div>
-      </div>
-      <!--home-->
-  <div class="text-center">
-    <form class="form-signin" action=""></form>
-    <h2>Inicia Sesión</h2>
-    <h6>Email</h6>
-    <label class="sr-only">Email address</label>
-    <input type="email" id="inputEmail" class="form-control" required="" autofocus="">
-    <button class="btn btn-primary" >Iniciar Sesión</button>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <Header/>
+    <Body/>
+    <Login/>
   </div>
 </template>
-
 <script>
+import Header from '../components/Header.vue';
+import Body from '../components/Body.vue';
+import Login from './Login.vue';
 
-  export default {
+export default {
   name: 'Home',
   components: {
+    Header,
+    Body,
+    Login,
   },
 };
 </script>
+<style>
+
+</style>
