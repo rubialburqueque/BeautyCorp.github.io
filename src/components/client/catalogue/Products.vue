c21fa80a90ba1f93f8f30db9d6567963538eb232
rubialburqueque <rubi.vap@gmail.com>
2020-04-16 10:56:12 -0500

    carrusel 2 vistas

--- src/components/client/catalogue/Products.vue
+++ src/components/client/catalogue/Products.vue
@@ -1,10 +1,30 @@
 <template>
-  <div>hola desde prod</div>
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="catalogo esika.jpg" class="d-block w-100" alt="40%">
+    </div>
+    <div class="carousel-item">
+      <img src="catalogo foto.jpg" class="d-block w-100" alt="40%">
+    </div>
+    <div class="carousel-item">
+      <img src="cyzonne.jpg" class="d-block w-100" alt="40%">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 </template>
 
 <script>
 export default {
-  name: Products,
+  name: 'Products',
 };
 </script>
 
