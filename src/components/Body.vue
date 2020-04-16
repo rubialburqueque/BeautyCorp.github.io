d093b4a86148191123c3e201ccea519ec4c35885
rubialburqueque <rubi.vap@gmail.com>
2020-04-16 13:03:37 -0500

    hacer mtach

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,30 +1,7 @@
 <template>
 <div>hola desde body
   <Products/>
-          <div class="select is-rounded is-small">
-            <select>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-              <option value="11">11</option>
-              <option value="12">12</option>
-              <option value="13">13</option>
-              <option value="14">14</option>
-              <option value="15">15</option>
-            </select>
-           </div>
-            <span class="title is-4">
-            <strong>S/. 49.90</strong>
-            </span>
-            <img src="@/assets/star.svg" alt="50x">
-</div>
+ </div>
 </template>
 
 <script>
--- src/components/client/catalogue/Products.vue
+++ src/components/client/catalogue/Products.vue
@@ -1,24 +1,39 @@
 <template>
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="catalogo esika.jpg" class="d-block w-100" alt="40%">
+<div >
+<ul class="list-group mb-3">
+  <li class="list-group-item d-flex justify-content-between lh-condensed">
+    <div>
+      <h6 class="my-0">Nombre del producto</h6>
+      <div>
+      <img src="@/assets/star.svg" alt="50x">
+      <img src="@/assets/star.svg" alt="50x">
+      <img src="@/assets/star.svg" alt="50x">
+      <img src="@/assets/star.svg" alt="50x">
+      </div>
+      <small class="text-muted">Descripcion del producto</small>
+               <div class="select is-rounded is-small">
+            <select>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+              <option value="13">13</option>
+              <option value="14">14</option>
+              <option value="15">15</option>
+            </select>
+           </div>
     </div>
-    <div class="carousel-item">
-      <img src="catalogo foto.jpg" class="d-block w-100" alt="40%">
-    </div>
-    <div class="carousel-item">
-      <img src="cyzonne.jpg" class="d-block w-100" alt="40%">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+    <strong>$20</strong>
+  </li>
+</ul>
 </div>
 </template>
 
