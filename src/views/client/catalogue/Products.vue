599e2dbf1482e9333b39cfb027b679d4a1d04d8a
jennlindley <jennifersu2059@gmail.com>
2020-04-16 15:28:26 -0500

    metiendo componente products a vista products

---
+++ src/views/client/catalogue/Products.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <Products/>
+  </div>
+</template>
+
+<script>
+import Products from '@/components/client/catalogue/Products.vue';
+
+export default {
+  name: 'Products',
+  components: {
+    Products,
+  },
+};
+</script>
+<style>
+
+</style>
