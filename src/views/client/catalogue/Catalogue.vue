7433ccdd3fc69dd5c40dec20a9a5f3d44c0c50b3
jennlindley <jennifersu2059@gmail.com>
2020-04-16 19:42:37 -0500

    mostrando vista catalogo y productos

---
+++ src/views/client/catalogue/Catalogue.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <ComponentCatalogue/>
+  </div>
+</template>
+
+<script>
+import ComponentCatalogue from '@/components/client/catalogue/Catalogue.vue';
+
+export default {
+  name: 'Products',
+  components: {
+    ComponentCatalogue,
+  },
+};
+</script>
+<style>
+
+</style>
