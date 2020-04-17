9bd79175dc348b26c7ec2fc92d32d1ec73be7b2b
naty4393 <naty4393@gmail.com>
2020-04-16 21:21:12 -0500

    update

--- src/components/consultant/catalogue/Offers.vue
+++ src/components/consultant/catalogue/Offers.vue
@@ -4,10 +4,12 @@
     <div class="container">
     <h2 class="h3 mb-3 font-weight-normal">Mis Beauty puntos</h2>
     <br>
+
     <br>
     <h6>Nivel 2</h6>
     <h1 class="card-body text-primary">Cantidad de puntos</h1>
-    <h6 class="card-subtitle mb-2 text-muted">A (cantidad de puntos)para llegar al tercer nivel</h6>
+    <h6 class="card-subtitle mb-2 text-muted">
+      A (cantidad de puntos) para llegar al tercer nivel</h6>
     <br>
     <br>
     <div class="checkbox mb-3">
